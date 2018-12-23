e94ea4f65cfc1d90552aebf3b5acd573ea3e043c
ClarkHo <593628543@qq.com>
2018-12-23 21:29:12 +0800

    Initial commit
    Initial commet

---
+++ src/views/Title.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    这是商品标题子组件
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg:'hello vue'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
