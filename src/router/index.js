2a45e905eb76a641c20982280c9a4db24f4e06d5
ClarkHo <593628543@qq.com>
2019-02-02 14:07:55 +0800

    13-1订单模块确认

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import GoodsList from './../views/GoodsList.vue'
 import Cart from '@/views/Cart'
+import Address from '@/views/Address'
+import OrderConfirm from '@/views/OrderConfirm'
 
 Vue.use(Router)
 
@@ -21,6 +23,16 @@ export let router = new Router({
       path: '/cart',
       name: 'Cart',
       component: Cart
+    },
+    {
+      path: '/address',
+      name: 'Address',
+      component: Address
+    },
+    {
+      path: '/OrderConfirm',
+      name: 'OrderConfirm',
+      component: OrderConfirm
     }
   ]
 })
